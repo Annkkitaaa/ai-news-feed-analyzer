82672292f4fe87ed95fcb630a0856899ce6645b7
Annkkitaaa <ankitasingh15.102@gmail.com>
2025-03-13 01:48:08 +0530

    updated dependencies.py
     Define reusable dependencies for API routes.

---
+++ backend/app/api/dependencies.py
@@ -0,0 +1,28 @@
+from typing import Generator
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+
+from app.db.session import get_db
+from app.core.config import settings
+from app.api.routes.auth import TokenPayload
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/auth/login"
+)
+
+def get_current_user_token_data(token: str = Depends(oauth2_scheme)) -> TokenPayload:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=["HS256"]
+        )
+        token_data = TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return token_data
